f74701471b4235021335da50fe66ac56f4924f15
mithmith <ivanivanov77681@gmail.com>
2025-02-18 00:28:36 +0300

    fixes

--- app/service/utils.py
+++ app/service/utils.py
@@ -5,7 +5,7 @@ from app.config import logger
 
 
 def clean_string(s: str) -> str:
-    allowed_chars = r"A-Za-zА-Яа-я0-9_\-"
+    allowed_chars = r"A-Za-zА-Яа-яЁё0-9_\-"
     return re.sub(rf"[^{allowed_chars}]", "", s)
 
 
